9c167e5354f194c78d28b2ab10ca495057bab454
Petercort <petercort@gmail.com>
2017-11-02 09:27:04 -0400

    test

--- private/encoder.php
+++ private/encoder.php
@@ -1,11 +1,9 @@
 <?php
 #encoder.php
-
 $servername = 'localhost';
 $username = 'pcort';
 $password = 'Ski23hik3';
 $dbname = 'MTGPimp';
-
 echo base64_encode($servername);
 echo "<br />";
 echo base64_encode($username);
