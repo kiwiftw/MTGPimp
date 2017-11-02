7fa455d201580fed4d7a49e51aed00e5b3e2e05d
Petercort <petercort@gmail.com>
2017-11-02 09:08:44 -0400

    doign a big update and likely screwing up source

--- searchResults.php
+++ searchResults.php
@@ -2,74 +2,47 @@
 ## SQL to pull results from the DB
 
 ## Declare Variables
+include("./Admin/functions.php");
 $setArray = [];
 $lastCardName = "";
-$config = parse_ini_file('../private/config.ini'); 
-$conn = mysqli_connect($config['servername'],$config['username'],$config['password'],$config['dbname']);
+$conn = getConnection();
+
  
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } else {
-	$value = $_POST['Decklist'];
-	if($value == ''){
+	$decklist = $_POST['Decklist'];
+	if($decklist == ''){
 		echo 'Decklist is empty, please enter some cards!';
 	} else {
 		$cardList = explode("\n", $value);
 		## Deck list was provided and broken up into an array. 
 		## Traverse the array and construct the sql query 
 		## The sql query is constructed as follows
-			## Count the card array, and create the query with the first value like so: 
-			## SELECT CardName, SetName FROM `cards` WHERE CardName = $cardName[0]
-			## For each other card name, add the card name in the sql query: $sqlExtension = 'OR CardName = $cardName[i]'
-			## Close the query with this final set $closer = 'Order By CardName Desc';
+		## Count the card array, and create the query with the first value like so: 
+		## SELECT CardName, SetName FROM `cards` WHERE CardName = $cardName[0]
+		## For each other card name, add the card name in the sql query: $sqlExtension = 'OR CardName = $cardName[i]'
+		## Close the query with this final set $closer = 'Order By CardName Desc';
 		## Close the DB connection and output the array into a nice html table. 
-		$safeCard = mysqli_real_escape_string($conn, preg_replace( "/\r|\n/", "", $cardList[0]));
-		$sql = "SELECT `CardName`, `SetName`, `Border`, `Foil`, `Language` FROM `Cards` WHERE `CardName` = '$safeCard'";
 
-		$cardCount = count($cardList);
-		for($i=1;$i<$cardCount;$i++){
-			$safeCard = mysqli_real_escape_string($conn, preg_replace( "/\r|\n/", "", $cardList[$i]));
-			$sql .= "OR `CardName` = '$safeCard'";
-		}
-		$sql .= "Order By CardName Desc;";
+		# For science, going to try and poll each card name, and return the output before going to the next card in the list.
+		# Could be that it's better to grab all the data at once and then output it once it's all been polled.  
+		# Something to look into and figure out if parallel would be something useful (not much knowledge about parallelism in php)
 
-		$cardArray = $conn->query($sql);
-
-		## End data needs to have Card Name => Set Name, PictureID, Language, Border, Foil
-		#$lastCardName = ucwords($cardList[0]);
-		#array_push($setArray, $lastCardName);
-		#echo $lastCardName;
-		while($data = mysqli_fetch_array($cardArray)){
-			## Create First Card Array
-			## If name is new then create a new array
-			if($lastCardName != $data["CardName"]){
-				$name = $data["CardName"];
-				$setName = $data["SetName"];
-				$setArray[$name][$setName]["Border"] = $data["Border"]; 
-				$setArray[$name][$setName]["Foil"] = $data["Foil"];
-				$setArray[$name][$setName]["Language"] = $data["Language"];
-				$lastCardName = $data["CardName"];
-			} else {
-				## else push data into arraye
-				$setName = $data["SetName"];
-				$setArray[$name][$setName]["Border"] = $data["Border"]; 
-				$setArray[$name][$setName]["Foil"] = $data["Foil"];
-				$setArray[$name][$setName]["Language"] = $data["Language"];
+		$decklist = explode(",",$decklist);
+		foreach($decklist as $value){
+			$returnedArray = getCard($conn, $value);
+			# Array will generally hold multiple cards in it, so grab the length of the array and poll through it.  
+			# This logic will change if I change to poll the entire input list at once to split different card names by line. 
+			# Should just need to implement one more loop at a higher level than this to check card names and new line if different
+			$cardName = $returnedArray[0]['CardName'];
+			echo $cardName . "<br />";
+			$cardCount = count($returnedArray);
+			for($x=0;$x<$cardCount;$x++){
+				outputCards($returnedArray[$x]);
 			}
 		}
-	}
-}
-$conn->close();
-foreach($setArray as $card => $allData){
-	echo $card . "<br/>";
-	foreach($allData as $set => $setData){
-		if($setData["Foil"] == 0){
-			$foil = "No";
-		} else { 
-			$foil = "Yes";
-		}
-		echo $set . " " . $setData["Border"]  . " " . $foil . " " . $setData["Language"] . "<br/>";
 	}
 }
 ?>
